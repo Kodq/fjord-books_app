e5fb1c573a34000bfa07df62ecdbe52f8d10c9fd
Kodq <s_riku@shiolab.com>
2022-10-27 22:50:28 +0900

    routesの無駄なコメントを削除、show.html.erbの修正、testファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, :only => [:index, :show]
-  #devise_for :users, :controllers => {
-    #:sessions      => "users/sessions",
-    #:registrations => "users/registrations",
-    #:passwords     => "users/passwords",
-  #}
-  #resources :users, :only => [:index, :show]
-  # ・・・
-  #get 'users/index'
-  #get 'users/show'
-  #devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
