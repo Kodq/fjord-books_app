5bf79a8f5e3c6605fc18cacf2459aa39bd02e11c
Kodq <s_riku@shiolab.com>
2022-11-13 13:05:53 +0900

    rubocopのエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+  has_many :follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :reverse_of_follows, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
 
   has_many :followings, through: :follows, source: :followed
   has_many :followers, through: :reverse_of_follows, source: :follower
@@ -16,10 +16,12 @@ class User < ApplicationRecord
   def follow(user_id)
     follows.create(followed_id: user_id)
   end
+
   # フォローを外すときの処理
   def unfollow(user_id)
     follows.find_by(followed_id: user_id).destroy
   end
+
   # フォローしているか判定
   def following?(user)
     followings.include?(user)
