47134031445eadaa63e83918e14917420bfcd66f
Kodq <s_riku@shiolab.com>
2022-12-17 03:16:17 +0900

    レビュー箇所の修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,13 +2,15 @@
 
 class FollowsController < ApplicationController
   def create
+    user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    redirect_to user
   end
 
   def destroy
+    user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    redirect_to user
   end
 
   def follower
